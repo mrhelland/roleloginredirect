27a04bf82142e0613ee815b8a85d690fa5be3319
Tim Helland <business@mr-math.com>
2025-10-19 14:55:52 -0700

    Update version.

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_roleloginredirect';
-$plugin->version   = 2025101200;
+$plugin->version   = 2025101900;
 $plugin->requires  = 2024072200;
 $plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '1.0.0';
+$plugin->release   = '1.0';
